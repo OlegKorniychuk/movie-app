21c01bf0247cdfcc0e1249528ebe2d24dc6fd9ed
OlegKorniychuk <kornoleg576@gmail.com>
2025-10-08 16:31:51 +0300

    feat: change movie service to return plain DTO objects

--- src/features/movies/movies.service.ts
+++ src/features/movies/movies.service.ts
@@ -1,38 +1,58 @@
-import { CreateMovieDto, UpdateMovieDto } from '../../core/dtos/movie.dto';
+import {
+  CreateMovieDto,
+  MovieResponseDto,
+  UpdateMovieDto,
+} from '../../core/dtos/movie.dto';
 import { Movie, MovieAttributes } from '../../core/models/movie.model';
 import { v4 as uuidv4 } from 'uuid';
 
 const moviesService = {
-  create: async (payload: CreateMovieDto): Promise<Movie> => {
-    return await Movie.create({
+  create: async (payload: CreateMovieDto): Promise<MovieResponseDto> => {
+    const newMovie = await Movie.create({
       id: uuidv4(),
       ...payload,
       actors: payload.actors.join(','),
     });
+    const plainMovie = newMovie.get({ plain: true });
+
+    return { ...plainMovie, actors: plainMovie.actors.split(',') };
   },
 
-  createMany: async (payload: CreateMovieDto[]): Promise<Movie[]> => {
-    const movies: MovieAttributes[] = payload.map((movie) => ({
+  createMany: async (
+    payload: CreateMovieDto[]
+  ): Promise<MovieResponseDto[]> => {
+    const createData: MovieAttributes[] = payload.map((movie) => ({
       id: uuidv4(),
       ...movie,
       actors: movie.actors.join(','),
     }));
+    const newMovies = await Movie.bulkCreate(createData);
+    const plainMovies = newMovies.map((m) => m.get({ plain: true }));
 
-    return await Movie.bulkCreate(movies);
+    return plainMovies.map((m) => ({ ...m, actors: m.actors.split(',') }));
   },
 
-  find: async (id: string): Promise<Movie | null> => {
-    return await Movie.findByPk(id);
+  find: async (id: string): Promise<MovieResponseDto | null> => {
+    const movie = await Movie.findByPk(id);
+
+    if (!movie) return null;
+
+    const plainMovie = movie.get({ plain: true });
+
+    return { ...plainMovie, actors: plainMovie.actors.split(',') };
   },
 
-  findMany: async (): Promise<Movie[]> => {
-    return await Movie.findAll();
+  findMany: async (): Promise<MovieResponseDto[]> => {
+    const newMovies = await Movie.findAll();
+    const plainMovies = newMovies.map((m) => m.get({ plain: true }));
+
+    return plainMovies.map((m) => ({ ...m, actors: m.actors.split(',') }));
   },
 
   update: async (
     id: string,
     payload: UpdateMovieDto
-  ): Promise<Movie | null> => {
+  ): Promise<MovieResponseDto | null> => {
     const { actors, ...rest } = payload;
     const patchedPayload: Omit<UpdateMovieDto, 'actors'> & { actors?: string } =
       { ...rest };
@@ -41,12 +61,18 @@ const moviesService = {
       patchedPayload.actors = actors.join(',');
     }
 
-    const updated = await Movie.update(patchedPayload, {
+    const result = await Movie.update(patchedPayload, {
       where: { id },
       returning: true,
     });
 
-    return updated[1][0] || null;
+    const updatedMovie = result[1][0];
+
+    if (!updatedMovie) return null;
+
+    const plainMovie = updatedMovie.get({ plain: true });
+
+    return { ...plainMovie, actors: plainMovie.actors.split(',') };
   },
 
   delete: async (id: string): Promise<number> => {
