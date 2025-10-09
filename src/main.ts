ff42159d95ffc809c96e36dfed0b6ba74a5e456b
OlegKorniychuk <kornoleg576@gmail.com>
2025-10-09 17:21:42 +0300

    fix: add handling for UniqueConstraintError

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import app from './app';
 import { sequelize } from './database';
 
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 8050;
 
 app.listen(port, async () => {
   await sequelize.sync();
